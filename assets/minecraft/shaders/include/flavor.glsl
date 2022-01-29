adb09e5aa85699c9ed0d51f8734644497d95d80e
SyndiShanX <brianmatthew.1026@gmail.com>
2022-01-29 18:59:38 -0500

    Full 1.18 Update

---
+++ assets/minecraft/shaders/include/flavor.glsl
@@ -0,0 +1,6 @@
+
+// Color of the outline on items when hovering over them in the GUI
+#define HOVER_OUTLINE_COLOR vec4(0.96, 0.843, 0.329, 1.0)
+
+// Color of the border around tooltips
+#define TOOLTIP_OUTLINE_COLOR vec4(0.96, 0.843, 0.329, 1.0)
