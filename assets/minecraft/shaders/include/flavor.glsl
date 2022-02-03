205ee2602b00572ae6305d83d3173b6ba6f459ad
Even Torset <eventorset@gmail.com>
2022-02-03 02:05:06 +0100

    Moved the loading screen background color to flavor.glsl.

--- assets/minecraft/shaders/include/flavor.glsl
+++ assets/minecraft/shaders/include/flavor.glsl
@@ -4,3 +4,7 @@
 
 // Color of the border around tooltips
 #define TOOLTIP_OUTLINE_COLOR vec4(0.96, 0.843, 0.329, 1.0)
+
+// Loading screen background color
+#define LOADING_BG_DARK_COLOR vec3(0.043, 0.141, 0.207)
+#define LOADING_BG_COLOR vec3(0.043, 0.141, 0.207)
