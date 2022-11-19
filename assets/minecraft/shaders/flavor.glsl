a242da40c6502f410e82a1bcb1d1c9a98038fc9d
Even Torset <eventorset@gmail.com>
2022-11-19 20:22:44 +0100

    Added the Cloudy End sky option.

--- assets/minecraft/shaders/flavor.glsl
+++ assets/minecraft/shaders/flavor.glsl
@@ -9,7 +9,7 @@
 #define LOADING_BG_DARK_COLOR vec3(0.043, 0.141, 0.207)
 #define LOADING_BG_COLOR vec3(0.043, 0.141, 0.207)
 
-// End sky colors
+// Otherworldy Rift End sky colors
 #define END_SKY_RIFT_COLOR vec3(0.1, 0.45, 0.9)
 
 #define END_SKY_STARS_OUTSIDE_BASE_COLOR vec3(0.4, 0.7, 0.9)
@@ -19,3 +19,14 @@
 #define END_SKY_STARS_INSIDE_BASE_COLOR vec3(1.2, 1.0, 2.4)
 #define END_SKY_STARS_INSIDE_COLOR_VARIANCE vec3(0.1, 0.1, 0.2)
 #define END_SKY_NEBULAE_INSIDE_COLOR vec3(0.5, 0.35, 0.9)
+
+// Cloudy End sky colors
+#define END_SKY_2_BASE_COLOR vec3(0.05, 0.067, 0.133)
+
+#define END_SKY_2_WEAK_CLOUDS_COLOR vec3(0.501, 0.549, 0.741)
+#define END_SKY_2_STRONG_CLOUDS_PRIMARY_COLOR vec3(0.819, 0.839, 0.925)
+#define END_SKY_2_STRONG_CLOUDS_COLOR_1 vec3(0.6, 0.4, 1.2)
+#define END_SKY_2_STRONG_CLOUDS_COLOR_2 vec3(0.7, 0.8, 1.6)
+
+#define END_SKY_2_STARS_BASE_COLOR vec3(0.8, 0.8, 0.9)
+#define END_SKY_2_STARS_COLOR_VARIANCE vec3(0.1, 0.1, 0.1)
